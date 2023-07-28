77f2ec7610a0986f3cc0eb5f127f51ae01d8630f
Shyam1hi <117503407+Shyam1hi@users.noreply.github.com>
2023-07-28 18:58:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-import streamli
+import streamlit as st
+import joblib
+
+#load the joblib model
+model_nb = joblib.load('spam-ham')
+
+st.title('SPAM HAM CLASSIFIER')
+ip = st.text_input('Enter your text :')
+
+op = model_nb.predict([ip])
+if st.button('PREDICT'):
+  st.title(op[0])
